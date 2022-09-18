b46b17b8382de74b1ab758334534208302ec4ea2
Elena Bruk <aen_law@mail.ru>
2022-09-18 15:40:51 +0300

    Second App

---
+++ SecondApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задание 1: Напишите программу, которая на вход принимвает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+if(a==b*b)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+Console.WriteLine("Первое число не является квадратом второго");
